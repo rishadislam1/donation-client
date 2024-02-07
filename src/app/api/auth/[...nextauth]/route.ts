f9cd39d27dcfd8108abe9e54941fdc508add9b91
rishadislam1 <rishad.islam158@gmail.com>
2024-02-07 09:46:37 +0600

    initial setup done. setup with next, nextauth, redux

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
+
+export { handler as GET, handler as POST };
